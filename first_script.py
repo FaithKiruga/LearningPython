1a09cd9c282f46026abe05f876d09ca073443b88
Faith Kiruga <faithnjerikiruga@gmail.com>
2022-07-17 15:28:41 +0300

    initial commit

---
+++ first_script.py
@@ -0,0 +1,26 @@
+my_age = 20
+price = 18.97
+print(my_age)
+print(price)
+
+#getting input from user
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+
+#CONVERTING DATA TYPE
+
+birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
+print("You are ", age , "years old")
+
+
+#SIMPLE CALCULATOR TEST
+
+x = input("Enter fist number: ")
+y = input("Enter second number: ")
+sum = float(x) + float(y)
+print("Sum: ", sum)
+
+
